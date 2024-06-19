de2c07a867a2f110410d2cc07e95ea5ecdeb7b2f
Danderson1989 <danieldouglasanderson@gmail.com>
2024-06-18 23:06:57 -0700

    added Oauth, accesstoken, and user search calls

--- src/App.js
+++ src/App.js
@@ -1,27 +1,59 @@
-import React, { useState } from 'react';
+import { React, useState, useEffect } from 'react';
 import './App.css';
 import SearchBar from './SearchBar';
 import SearchResults from './SearchResults';
 import Playlist from './Playlist';
-import { v4 as uuidv4 } from 'uuid';
+import { requestUserAuth, getAccessToken, trackSearch,  } from './accessToken'
 
 function App() {
 
-const [searchResults, setSearchResults] = useState([
-    {artist: 'Dan', song: 'Dan\'s song', album: 'Dan\'s album', id: 0 },
-    {artist: 'Dylan', song: 'Dylan\'s song', album: 'Dylan\'s album', id: 1 },
-    {artist: 'Christy', song: 'Christy\'s song', album: 'Christy\'s album', id: 2 }
-]);
+const clientId = "318706de36214725959933a3c1369354";
+const clientSecret = '0aa627dcc3c54d06ba84a289bfd6fbcb';
+const redirectUri = 'http://localhost:3000/';
 
+const [code, setCode] = useState(null);
+const [accessToken, setAccessToken] = useState(null);
+const [searchResults, setSearchResults] = useState({});
 const [search, setSearch] = useState("");
-
 const [playlistName, setPlaylistName] = useState("");
-
 const [addedTracks, setAddedTracks] = useState([]);
+
+//Authorize App to use make changes to clients Spotify account
+useEffect(()=> {
+  //Check for code in URL
+  const params = new URLSearchParams(window.location.search);
+  const codeFromUrl = params.get('code');
+
+  if (!codeFromUrl) {
+    requestUserAuth(clientId, redirectUri);
+  } 
+
+  setCode(codeFromUrl);
+}, []);
+
+// Get access Token
+useEffect(()=> {
+  async function fetchAccessToken() {
+    if (code) {
+      try {
+        const token = await getAccessToken(code, clientId, clientSecret, redirectUri);
+        setAccessToken(token);
+      } catch (error) {
+        console.log("Error fetching access token:", error);
+      }
+    } 
+  }
+
+fetchAccessToken();
+}, [code]);
 
 const handleSearch = e => {
     e.preventDefault();
-    alert(search);
+    async function fetchSearchResults() {
+        const userSearch =  await trackSearch(search, accessToken.access_token);
+        setSearchResults(userSearch);
+    }
+    fetchSearchResults();
 };
 
 const handleInputChange = e => {
@@ -32,18 +64,17 @@ const handlePlaylistName = e => {
     setPlaylistName(e.target.value);
 }
 
-const handleAddedTracks = (trackObj) => {
-    const tracklistKey = uuidv4();
+const handleAddedTracks = (artist, album, track, id, uri) => {
+    const trackToAdd = {artist: artist, album: album, track: track, id: id, uri: uri}
     setAddedTracks([
         ...addedTracks,
-        {...trackObj, tracklistKey: tracklistKey}
+        trackToAdd
     ]);
- 
 }
 
-const handleRemovingTracks = (tracklistKey) => {
+const handleRemovingTracks = (id) => {
     setAddedTracks(addedTracks.filter(track => 
-        track.tracklistKey !== tracklistKey
+        track.id !== id
     ))
 }
 
@@ -59,6 +90,7 @@ return (
         <SearchResults
             searchResults={searchResults}
             handleAddedTracks={handleAddedTracks}
+            trackSearch = {trackSearch}
         />
         <Playlist 
             playlistName={playlistName}
@@ -73,3 +105,4 @@ return (
 };
 
 export default App;
+
